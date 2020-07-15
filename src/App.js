5fcea68e0bb1658618d532050cb1ac73a47cf61c
Souradip Mookerjee <souramoo@gmx.com>
2020-07-15 03:09:34 +0100

    fix tile layer again

--- src/App.js
+++ src/App.js
@@ -168,7 +168,7 @@ Select a place below for more:</Typography>
 Map:</Typography><br />
                <Map center={[52.2053, 0.1218]} zoom={13} minZoom={12} maxZoom={17} style={{width: "100%", height: "500px"}}>
     <TileLayer
-      url="https://a.tile.openstreetmap.org/${z}/${x}/${y}.png"
+      url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
       attribution=""
     />
 
@@ -309,7 +309,7 @@ this.props.dispatch({type: "SET_H2", title: item.name})
 <Paper style={{ padding: "20px 50px" }}>
                <Map center={item.position} zoom={15} minZoom={15} maxZoom={15} zoomControl={false} style={{width: "100%", height: "200px"}}>
     <TileLayer
-      url="https://a.tile.openstreetmap.org/${z}/${x}/${y}.png"
+      url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
       attribution=""
     />
     <Marker position={item.position}>
